6f78c958283c4e2c3343163259c54973b2720d01
Rilex <vic285706@gmail.com>
2026-04-24 11:37:02 -0300

    usuarioAPI pronto

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -3,3 +3,37 @@ using MinhaLojaApi.Models;
 
 namespace MinhaLojaApi.Data;
 
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+     
+    }
+
+    public DbSet<Usuario> Usuario { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema("PREPENEM");
+
+        // telaUSUARIO
+        modelBuilder.Entity<Usuario>().HasKey(u => u.IdUsuario);
+
+        modelBuilder.Entity<Usuario>()
+            .Property(u => u.Email)
+            .HasColumnType("varchar(40)");
+
+        modelBuilder.Entity<Usuario>()
+            .Property(u => u.Senha)
+            .HasColumnType("char(6)");
+
+        modelBuilder.Entity<Usuario>()
+            .Property(u => u.FotoPerfil)
+            .HasColumnType("varchar(100)");
+
+       
+
+    }
+}
